cd3ec423a532c8281c642ad402a1484053e5b14b
Alec Ventura <alecventura@hotmail.com>
2017-08-31 20:05:52 -0300

    Products retrieve using jsonp ok

---
+++ app/js/index.js
@@ -0,0 +1,34 @@
+const PRODUCTS_URL = 'http://roberval.chaordicsystems.com/challenge/challenge.json';
+
+const main = () => {
+  const getJSONP = (url, callback) => {
+    const script = document.createElement('script');
+    const head = document.getElementsByTagName('head')[0] || document.documentElement;
+
+    window.X = (data) => {
+      head.removeChild(script);
+      if (callback) callback(data);
+    };
+
+    script.src = url;
+    head.appendChild(script);
+  };
+
+  const renderProducts = () => {
+
+  };
+
+  const getProductList = () => {
+    getJSONP(PRODUCTS_URL, (data) => {
+      renderProducts(data);
+    });
+  };
+
+  const init = () => {
+    getProductList();
+  };
+
+  init();
+};
+
+main();
